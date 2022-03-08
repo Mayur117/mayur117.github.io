c38b50cb25d21c72d17b7e47b845f626478acfb5
Mayur Thummar <41770235+Mayur117@users.noreply.github.com>
2022-03-08 15:29:41 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+////////////////////
+// Scroll Reveal
+
+ScrollReveal().reveal('.scroll-one', { delay: 200, easing: 'ease-in' });
+ScrollReveal().reveal('.scroll-two', { delay: 500, easing: 'ease-in' });
+ScrollReveal().reveal('.scroll-three', { delay: 800, easing: 'ease-in' });
+ScrollReveal().reveal('.scroll-four', { delay: 1000, easing: 'ease-in' });
